7dec4a2493036ee51a23989f56118065a66e004a
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-12-17 14:07:19 -0800

    created types and classes for Player and Enemy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,49 @@
 // types
-type Competitor = {
-    
+interface Competitor {
+    name: string,
+    health: number,
+    attack: number
+}
+
+type PlayerProps = {
+    name: string,
+    health: number,
+    attack: number,
+    money: number
+}
+
+type EnemyProps = {
+    name: string,
+    health: number,
+    attack: number
 }
 
 // classes
+class Player implements Competitor {
+    name: string;
+    health: number;
+    attack: number;
+    money: number;
+    constructor(props: PlayerProps) {
+        this.name = props.name;
+        this.health = props.health;
+        this.attack = props.attack;
+        this.money = props.money;
+    }
+}
+
+class Enemy implements Competitor {
+    name: string;
+    health: number;
+    attack: number;
+    constructor(props: EnemyProps) {
+        this.name = props.name;
+        this.health = props.health;
+        this.attack = props.attack;
+    }
+}
 
 // execution
-window.alert('hello')
+const enemy = new Enemy({name:'Roborto', health: 75, attack: 12});
+const player = new Player({name:'Sydney', health: 75, attack: 12, money: 100});
+console.log(player)
